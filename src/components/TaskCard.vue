ba516f31845f3f36604028a87d1c93dadc8adb4d
ExcelDurant <gnopaexcel@yahoo.fr>
2021-11-08 19:23:45 +0100

    started working on the task page

---
+++ src/components/TaskCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="task-card">
+        <div class="img-container">
+            <img src="https://images.unsplash.com/photo-1634227555537-dfedab448cf3?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=492&q=80" alt="" class="full-img">
+        </div>
+        <div class="top-row">
+            <h3 class="task-name">Help me repair my couch</h3>
+            <div class="reward-container">
+                <h5 class="reward">25000 FCFA</h5>
+            </div>
+        </div>
+        <h6 class="category">electrician</h6>
+        <div class="desc-container">
+            <p class="desc">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+                Repellendus modi aliquid impedit aliquam velit odit quasi, earum quaerat 
+                veritatis mollitia nihil consectetur doloremque dicta aut iste ut. 
+                Reprehenderit dolorum porro, molestiae voluptatum excepturi aliquam saepe recusandae!
+            </p>
+        </div>
+        <div class="bottom-row">
+            <h5 class="location"><i class="fas fa-map-marker"></i>Logpom, Douala, Cameroon</h5>
+            <h6 class="date">created 2 hours ago</h6>
+        </div>
+        <a href="#" class="details-link">view details</a>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
+
+<style lang="scss" scoped>
+@import "@/styles/_variables.scss";
+
+</style>
--- src/views/OfferHelp.vue
+++ src/views/OfferHelp.vue
@@ -1,12 +1,45 @@
 <template>
-    <h1>tasks page</h1>
+ <div class="filters-container">
+    <div class="filter-txt-container">
+      <h3 class="filter">filter by:</h3>
+    </div>
+    <div class="category-select-container">
+      <select class="form-select category-select select" aria-label="Default select example">
+        <option selected>select category</option>
+        <option value="1">driver</option>
+        <option value="2">dress washer</option>
+        <option value="3">engineer</option>
+        <option value="3">electrician</option>
+        <option value="3">engineer</option>
+        <option value="3">engineer</option>
+      </select>
+    </div>
+    <div class="location-select-container">
+      <select class="form-select location-select select" aria-label="Default select example">
+        <option selected>select location</option>
+        <option value="1">Douala</option>
+        <option value="2">Yaounde</option>
+        <option value="3">Bafoussam</option>
+        <option value="3">Bamenda</option>
+        <option value="3">Buea</option>
+        <option value="3">Kribi</option>
+      </select>
+    </div>
+  </div>
+    <section class="tasks-sec">
+        <div class="tasks-container">
+            <task-card></task-card>
+        </div>
+    </section>
 </template>
 
 <script lang="ts">
 import { navbarState } from '@/services/navbar';
-import { defineComponent } from 'vue'
+import { defineComponent } from 'vue';
+import TaskCard from '@/components/TaskCard.vue';
 
 export default defineComponent({
+    components: {TaskCard},
     mounted() {
     navbarState.changeTitle(" - Offer Help");
     navbarState.changeAuth(false);
@@ -20,4 +53,46 @@ export default defineComponent({
 
 <style lang="scss" scoped>
 @import "@/styles/_variables.scss";
+
+.filters-container {
+  width: 90%;
+  margin: 0 auto;
+  margin-top: 10px;
+  margin-bottom: 20px;
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+}
+
+.filter-txt-container {
+  .filter {
+    font-weight: 300;
+    font-size: 15px;
+  }
+}
+
+.select {
+  width: 100%;
+    border-radius: 15px;
+    box-shadow: 0px 4px 8px 1px rgb(221, 221, 221);
+}
+.category-select-container {
+  width: 350px;
+  .category-select {
+    width: 100%;
+    border-radius: 15px;
+  }
+}
+
+.location-select-container {
+  width: 200px;
+  .location-select {
+    width: 100%;
+    border-radius: 15px;
+  }
+} 
+
+.language-select-container {
+  width: 200px;
+}
 </style>
